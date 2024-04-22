43edf01d0ddfc07d0de9d8cfc0feac579d246121
Enea Guarneri <eneag98@gmail.com>
2024-04-22 13:48:26 +0200

    - DB and TextFile put in "data" folder - executable file "Rubrica.jar" in "artifacts" folder inner path

--- src/Rubrica.java
+++ src/Rubrica.java
@@ -4,6 +4,6 @@ import backend.Rubrica_DATA;
 public class Rubrica {
 
     public static void main(String[] args) {
-        new Rubrica_DATA("src/backend/informazioni.db", "src/backend/informazioni.txt");
+        new Rubrica_DATA("data/informazioni.db", "data/informazioni.txt");
     }
 }
